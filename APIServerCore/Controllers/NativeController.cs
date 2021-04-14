baed56945768cfc459bf7b484a4bccc54ff4cb8c
Remi THOMAS <remi@xtware.com>
2021-04-14 08:28:26 +0200

    First commit

---
+++ APIServerCore/Controllers/NativeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace APIServerCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NativeController : ControllerBase
+    {
+        private readonly ILogger<NativeController> _logger;
+
+        public NativeController(ILogger<NativeController> logger)
+        {
+            _logger = logger;
+        }
+
+        delegate int AddDelegate(int a, int b);
+
+        [HttpGet("{a}/{b}")]
+        public IActionResult Get([FromRoute]int a, [FromRoute]int b)
+        {
+            IntPtr ptr = NativeLibrary.Load("Dll1.dll");
+            if (ptr != IntPtr.Zero)
+            {
+                if (NativeLibrary.TryGetExport(ptr, "Add", out IntPtr AddPtr))
+                {
+                    var _add = (AddDelegate)Marshal.GetDelegateForFunctionPointer(AddPtr, typeof(AddDelegate));
+                    return Ok(new { a = a, b = b, result = _add(a, b) });
+                }
+            }
+            return NotFound();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ExposeCppApi
-Expose C++ to API using C# dotnet framework server - demo
+Expose C++ to API using C# dotnet core server - demo
+
+There is 3 projects
+
+The native DLL demo that expose in Add(int, int)
+
+The API server that accept
+
+> http://localhost:5000/Native/1/2
+ 
+and returns
+
+> {
+  "a": 1,
+  "b": 2,
+  "result": 3
+}
+
